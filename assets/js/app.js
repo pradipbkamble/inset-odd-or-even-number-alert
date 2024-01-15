83e2480231c5f1ff7fda474d85617795347930c3
pradipbkamble <pradipbkamble001@gmail.com>
2024-01-16 02:10:16 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const cl=console.log;
+
+
+const btn1=document.getElementById("btn1");
+const input1=document.getElementById("input1");
+
+
+
+
+
+
+let onbtnclick=(eve)=>{
+cl(eve)
+let oddeve=input1.value; 
+cl(oddeve);
+
+// if(oddeve===0){
+//     alert(`${oddeve} invalid`)
+// }
+if(oddeve%2===0){
+    alert(`${oddeve} is even`)
+}
+else{
+    alert(`${oddeve} is odd`)
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+btn1.addEventListener("click",onbtnclick);
+
+
